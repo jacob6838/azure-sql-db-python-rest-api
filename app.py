44b5a2ef34308dbdcb171c7b1e4d30bf02496283
jacob6838 <51723791+jacob6838@users.noreply.github.com>
2021-02-25 22:39:07 -0700

    creating simple REST API

--- app.py
+++ app.py
@@ -1,129 +1,51 @@
-import sys
-import os
-import json
-import pyodbc
-import socket
-from flask import Flask
-from flask_restful import reqparse, abort, Api, Resource
-from threading import Lock
-from tenacity import *
-from opencensus.ext.azure.trace_exporter import AzureExporter
-from opencensus.ext.flask.flask_middleware import FlaskMiddleware
-from opencensus.trace.samplers import ProbabilitySampler
-import logging
+from flask import Flask, request
+from azure.storage.blob import BlobServiceClient
+from flask_restful import Resource, Api
 
-# Initialize Flask
 app = Flask(__name__)
-
-# Setup Azure Monitor
-if 'APPINSIGHTS_KEY' in os.environ:
-    middleware = FlaskMiddleware(
-        app,
-        exporter=AzureExporter(connection_string="InstrumentationKey={0}".format(os.environ['APPINSIGHTS_KEY'])),
-        sampler=ProbabilitySampler(rate=1.0),
-    )
-
-# Setup Flask Restful framework
 api = Api(app)
-parser = reqparse.RequestParser()
-parser.add_argument('customer')
 
-# Implement singleton to avoid global objects
-class ConnectionManager(object):    
-    __instance = None
-    __connection = None
-    __lock = Lock()
+conn_str = 'DefaultEndpointsProtocol=https;AccountName=neaeraiotstorage;AccountKey=gSFq2szM88ag0BV/J7QqzoXdak1aIGsUgyWagsR/96mlVnQhdOTnns6D7z8nOgRUdQy3FdbMxEmufrCqmE6mdw==;EndpointSuffix=core.windows.net'
+blob_service_client = BlobServiceClient.from_connection_string(conn_str)
 
-    def __new__(cls):
-        if ConnectionManager.__instance is None:
-            ConnectionManager.__instance = object.__new__(cls)        
-        return ConnectionManager.__instance       
-    
-    def __getConnection(self):
-        if (self.__connection == None):
-            application_name = ";APP={0}".format(socket.gethostname())  
-            self.__connection = pyodbc.connect(os.environ['SQLAZURECONNSTR_WWIF'] + application_name)                  
-        
-        return self.__connection
+class RSM_XML(Resource):
+    def get(self, rsm_name):
+        container_name = 'publishedworkzones'
+        blob_name = 'rsm-xml/' + rsm_name
+        blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
+        return {'data': blob_client.download_blob().readall().decode('utf-8')}
 
-    def __removeConnection(self):
-        self.__connection = None
+class RSM_XML_LIST(Resource):
+    def get(self):
+        container_name = 'publishedworkzones'
+        container_client = blob_service_client.get_container_client(container_name)
+        blob_list = container_client.list_blobs(name_starts_with='rsm-xml/')
+        blob_names = []
+        for blob in blob_list:
+            blob_names.append({'name': blob.name, 'etag': blob.etag})
+        return {'data': blob_names}
 
-    @retry(stop=stop_after_attempt(3), wait=wait_fixed(10), retry=retry_if_exception_type(pyodbc.OperationalError), after=after_log(app.logger, logging.DEBUG))
-    def executeQueryJSON(self, procedure, payload=None):
-        result = {}  
-        try:
-            conn = self.__getConnection()
+class RSM_UPER(Resource):
+    def get(self, rsm_name):
+        container_name = 'publishedworkzones'
+        blob_name = 'rsm-uper/' + rsm_name
+        blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
+        return {'data': str(blob_client.download_blob().readall())}
 
-            cursor = conn.cursor()
-            
-            if payload:
-                cursor.execute(f"EXEC {procedure} ?", json.dumps(payload))
-            else:
-                cursor.execute(f"EXEC {procedure}")
+class RSM_UPER_LIST(Resource):
+    def get(self):
+        container_name = 'publishedworkzones'
+        container_client = blob_service_client.get_container_client(container_name)
+        blob_list = container_client.list_blobs(name_starts_with='rsm-uper/')
+        blob_names = []
+        for blob in blob_list:
+            blob_names.append({'name': blob.name, 'etag': blob.etag})
+        return {'data': blob_names}
 
-            result = cursor.fetchone()
+api.add_resource(RSM_XML, '/rsm/xml/<string:rsm_name>')
+api.add_resource(RSM_XML_LIST, '/rsm/xml-list')
+api.add_resource(RSM_UPER, '/rsm/uper/<string:rsm_name>')
+api.add_resource(RSM_UPER_LIST, '/rsm/uper-list')
 
-            if result:
-                result = json.loads(result[0])                           
-            else:
-                result = {}
-
-            cursor.commit()    
-        except pyodbc.OperationalError as e:            
-            app.logger.error(f"{e.args[1]}")
-            if e.args[0] == "08S01":
-                # If there is a "Communication Link Failure" error, 
-                # then connection must be removed
-                # as it will be in an invalid state
-                self.__removeConnection() 
-                raise                        
-        finally:
-            cursor.close()
-                         
-        return result
-
-class Queryable(Resource):
-    def executeQueryJson(self, verb, payload=None):
-        result = {}  
-        entity = type(self).__name__.lower()
-        procedure = f"web.{verb}_{entity}"
-        result = ConnectionManager().executeQueryJSON(procedure, payload)
-        return result
-
-# Customer Class
-class Customer(Queryable):
-    def get(self, customer_id):     
-        customer = {}
-        customer["CustomerID"] = customer_id
-        result = self.executeQueryJson("get", customer)   
-        return result, 200
-    
-    def put(self):
-        args = parser.parse_args()
-        customer = json.loads(args['customer'])
-        result = self.executeQueryJson("put", customer)
-        return result, 201
-
-    def patch(self, customer_id):
-        args = parser.parse_args()
-        customer = json.loads(args['customer'])
-        customer["CustomerID"] = customer_id        
-        result = self.executeQueryJson("patch", customer)
-        return result, 202
-
-    def delete(self, customer_id):       
-        customer = {}
-        customer["CustomerID"] = customer_id
-        result = self.executeQueryJson("delete", customer)
-        return result, 202
-
-# Customers Class
-class Customers(Queryable):
-    def get(self):     
-        result = self.executeQueryJson("get")   
-        return result, 200
-    
-# Create API routes
-api.add_resource(Customer, '/customer', '/customer/<customer_id>')
-api.add_resource(Customers, '/customers')
+# if __name__ == '__main__':
+#     app.run() #debug=True
